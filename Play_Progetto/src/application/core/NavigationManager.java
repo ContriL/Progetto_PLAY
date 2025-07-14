b334d8125ab4b5eb22b7e09bf866190e91a9603b
mattiaveroniunibo <mattia.veroni@studio.unibo.it>
2025-07-14 23:28:37 +0200

    risolto problema dimensione pagine
    risolto problema dimensione pagine, ora quando cambiamo schermata la pagina rimane della stessa dimensione che noi scegliamo grazie a mantaincurrentsize()

--- Play_Progetto/src/application/core/NavigationManager.java
+++ Play_Progetto/src/application/core/NavigationManager.java
@@ -26,6 +26,10 @@ public class NavigationManager {
     private Stage primaryStage;
     private Map<String, Scene> cachedScenes = new HashMap<>();
 
+    private double currentWidth = 1200;
+    private double currentHeight = 800;
+    private boolean isMaximized = false;
+
     private NavigationManager() {}
 
     public static NavigationManager getInstance() {
@@ -37,6 +41,39 @@ public class NavigationManager {
 
     public void initialize(Stage stage) {
         this.primaryStage = stage;
+
+        // Mantieni le dimensioni quando cambiano
+        stage.widthProperty().addListener((obs, oldVal, newVal) -> {
+            if (!stage.isMaximized()) {
+                currentWidth = newVal.doubleValue();
+            }
+        });
+
+        stage.heightProperty().addListener((obs, oldVal, newVal) -> {
+            if (!stage.isMaximized()) {
+                currentHeight = newVal.doubleValue();
+            }
+        });
+
+        // Salva stato massimizzato
+        stage.maximizedProperty().addListener((obs, oldVal, newVal) -> {
+            isMaximized = newVal;
+        });
+    }
+
+    /**
+     * Forza il mantenimento delle dimensioni correnti
+     */
+    private void maintainCurrentSize() {
+        if (primaryStage != null) {
+            if (isMaximized) {
+                primaryStage.setMaximized(true);
+            } else {
+                primaryStage.setMaximized(false);
+                primaryStage.setWidth(currentWidth);
+                primaryStage.setHeight(currentHeight);
+            }
+        }
     }
 
     public Stage getPrimaryStage() {
@@ -48,22 +85,27 @@ public class NavigationManager {
     public void goToHome() {
         Scene homeScene = Home.getScene(primaryStage, getLoginScene());
         primaryStage.setScene(homeScene);
+        maintainCurrentSize();
+
     }
 
     public void goToLogin() {
         Scene loginScene = getLoginScene();
         primaryStage.setScene(loginScene);
+        maintainCurrentSize();
     }
 
     public void goToExerciseGrid() {
         Scene gridScene = ExerciseGridScreen.createScene(primaryStage);
         primaryStage.setScene(gridScene);
+        maintainCurrentSize();
     }
 
     public void goToUserProgress() {
         Scene currentScene = primaryStage.getScene();
         Scene progressScene = UserProgressScreen.getScene(primaryStage, currentScene);
         primaryStage.setScene(progressScene);
+        maintainCurrentSize();
     }
 
     public void goToProfile() {
@@ -72,6 +114,7 @@ public class NavigationManager {
         // Chiama createScene() senza parametri
         Scene profileScene = profileScreen.createScene();
         primaryStage.setScene(profileScene);
+        maintainCurrentSize();
     }
 
 
@@ -80,12 +123,14 @@ public class NavigationManager {
         Scene rulesScene = application.screens.exercises.ExerciseRulesScreen.createScene(
                 primaryStage, exercise, returnTo);
         primaryStage.setScene(rulesScene);
+        maintainCurrentSize();
     }
 
     
     public void startExercise(Exercise exercise, String returnTo) {
         Scene exerciseScene = createExerciseScene(exercise);
         primaryStage.setScene(exerciseScene);
+        maintainCurrentSize();
     }
 
     public void logout() {
